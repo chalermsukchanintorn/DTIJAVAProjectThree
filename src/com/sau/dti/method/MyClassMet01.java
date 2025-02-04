819b24370e7f84b6cb275773b00185f65776ddca
chalermsukchanintorn <chalermsukchanintorn@gmail.com>
2025-02-04 16:22:51 +0700

    complete method

--- src/com/sau/dti/method/MyClassMet01.java
+++ src/com/sau/dti/method/MyClassMet01.java
@@ -1,7 +1,23 @@
 package com.sau.dti.method;
 
 public class MyClassMet01 {
-    public static void main(String[] args) {
+    //method คือ function คือ การทำงานหนึ่งๆ ที่จะไม่จะไม่ทำหากไม่เรียกใช้ (call method)
+    public static void showHi( ){
+        System.out.println("Hi....");
+    }
 
+    public static void showHello( ){
+        System.out.println("Hello....");
+    }
+
+    public static void main( String[] args ) {
+        showHi();
+        showHi();
+        showHey();
+    }
+
+    public static void showHey( ){
+        System.out.println("Hey....");
+        showHi();
     }
 }
---
+++ src/com/sau/dti/method/MyClassMet03.java
@@ -0,0 +1,16 @@
+package com.sau.dti.method;
+
+public class MyClassMet03 {
+    //Overloading method
+    public static void showH(){ }
+
+    public static void showH(int a){ }
+
+    public static void showH(double a){ }
+
+    public static void showH(double a, int b, String c){ }
+
+    public static void main(String[] args) {
+
+    }
+}
